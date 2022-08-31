66f15ff05ff813193d9b1ba153dbb4b05c41cfd3
Joe Contreras <joe.contreras888@gmail.com>
2022-08-31 17:30:23 -0500

    Add Authentication middleware and user-posts relationship

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -4,9 +4,11 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { User } from "./User";
 
 @ObjectType()
 @Entity()
@@ -26,4 +28,19 @@ export class Post extends BaseEntity {
   @Field(() => String)
   @Column()
   title!: string;
+
+  @Field(() => String)
+  @Column()
+  text!: string;
+
+  @Field(() => Int)
+  @Column({ type: "int", default: 0 })
+  points!: string;
+
+  @Field(() => String)
+  @Column()
+  creatorId: number;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  creator: User;
 }
